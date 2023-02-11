171b668ddd29b5f136bc6773d4b984d096a236a0
Edson Sales <edsondesouzasales@gmail.com>
2023-02-11 05:33:26 +0000

    fix/Conserta bug dos controllers

--- src/questao/2-certo-ou-errado/certo-ou-errado.module.ts
+++ src/questao/2-certo-ou-errado/certo-ou-errado.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CertoOuErradoService } from './certo-ou-errado.service';
-import { CertoOuErradoController } from './certo-ou-errado.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [CertoOuErradoController],
   providers: [CertoOuErradoService],
   exports: [CertoOuErradoService],
 })
--- src/questao/3-valor-exato/valor-exato.module.ts
+++ src/questao/3-valor-exato/valor-exato.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ValorExatoService } from './valor-exato.service';
-import { ValorExatoController } from './valor-exato.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [ValorExatoController],
   providers: [ValorExatoService],
   exports: [ValorExatoService],
 })
