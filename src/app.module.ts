baba94a373fc8e8463925d8f8951f75bb7b1c7c0
Edson Sales <edsondesouzasales@gmail.com>
2023-02-12 16:14:53 -0300

    feat/Adiciona configuração

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { QuestaoModule } from './questao/questao.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, QuestaoModule],
+  imports: [PrismaModule, QuestaoModule, AuthModule, ConfigModule.forRoot({isGlobal: true})],
   controllers: [AppController],
   providers: [AppService],
 })
