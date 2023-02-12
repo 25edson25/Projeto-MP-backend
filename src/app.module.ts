33a91c250ced34d092521075b0bcb62e960b882f
Edson Sales <edsondesouzasales@gmail.com>
2023-02-12 04:59:18 +0000

    corrige bugs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { QuestaoModule } from './questao/questao.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, QuestaoModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use();
-  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
