b7e87a725145f57c441972108755c0e0c8566b6f
Edson Sales <edsondesouzasales@gmail.com>
2023-02-13 02:33:22 -0300

    refactor/refatora findAll do usuario

--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CrudOptions, RejectOptions } from '@cjr-unb/super-crud';
-import { Prisma } from '@prisma/client';
+import { Prisma, Usuario } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
+import { roles } from 'src/app.role';
 
 type UsuarioModel = Prisma.UsuarioDelegate<RejectOptions>;
 const { defaultOptions, getCrud } = new CrudOptions<UsuarioModel>().setOptions(
@@ -15,7 +16,11 @@ export class UsuarioService extends getCrud<
   constructor(protected readonly prisma: PrismaService) {
     super(prisma.usuario, defaultOptions);
   }
-  
+
+  async findAll() {
+    return await this.prisma.usuario.findMany({include:{roles:true}})
+  }
+
   async findByEmail(email: string) {
     const user = await this.prisma.usuario.findUnique({
       where: { email },
