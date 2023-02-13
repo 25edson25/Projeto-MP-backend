187ae462625d2f53dfb67c5974a7b5178be3df9d
Edson Sales <edsondesouzasales@gmail.com>
2023-02-13 02:17:32 -0300

    feat/adiciona validators em user entity

--- src/usuario/entities/usuario.entity.ts
+++ src/usuario/entities/usuario.entity.ts
@@ -1,9 +1,16 @@
 import { Usuario } from '@prisma/client';
+import { IsDate, IsEmail, IsString } from 'class-validator';
+
 
 export class UsuarioEntity implements Usuario {
+  @IsString()
   cpf: string;
+  @IsString()
   nome: string;
+  @IsEmail()
   email: string;
+  @IsString()
   senha: string;
+  @IsDate()
   dataNascimento: Date;
 }
--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,23 +1,14 @@
 import {
   Controller,
   Get,
-  Post,
-  Body,
-  Patch,
   Param,
   Delete,
 } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
-import { CreateUsuarioDto } from './dto/create-usuario.dto';
 
 @Controller('usuario')
 export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
-
-  @Post()
-  create(@Body() createUsuarioDto: CreateUsuarioDto) {
-    return this.usuarioService.create(createUsuarioDto);
-  }
 
   @Get()
   findAll() {
